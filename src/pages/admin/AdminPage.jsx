c30ca8bc4330b0138613b1fa84a523569077e48f
Santino <srossiprieto@gmail.com>
2024-10-30 15:38:34 -0300

    feat: admin-crud

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -2,22 +2,14 @@ import React from 'react'
 import { AdminContainer } from '../../pages/admin/AdminPageStyles'
 // import CreateCategory from '../../components/adminComponents/CreateCategory/CreateCategory'
 // import ListProducts from '../../components/adminComponents/ListProducts/ListProducts'
-import CreateProduct from '../../components/adminComponents/CreateProduct/CreateProduct'
+import ListProducts from '../../components/adminComponents/ListProducts/ListProducts'
 
 
 function AdminPage() {
   return (
     <>
       <AdminContainer>
-<<<<<<< HEAD
-        {/* <CreateCategory />
-        <ListProducts/> */}
-          <CreateProduct />
-
-=======
-        {/* <CreateCategory /> */}
         <ListProducts/> 
->>>>>>> a323870d7a0882540af0134866d5a896dd969af3
       </AdminContainer>
     </>
   )
