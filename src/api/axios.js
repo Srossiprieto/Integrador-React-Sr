1ca65d5f7f8208aa4f62f4c3229cc91502b8c2bf
Santino <srossiprieto@gmail.com>
2024-10-31 12:30:19 -0300

    feat: axios-config

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,23 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  withCredentials: true,
+  withCredentials: true  // Asegúrate de que las credenciales se envíen con cada solicitud
 });
+
+// Interceptor para agregar el token a cada solicitud
+instance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
